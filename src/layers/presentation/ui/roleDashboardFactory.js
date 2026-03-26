d7c9091821226d32f739934a1833734fe7e53acc
Kevin <kevinkerr1234@proton.me>
2026-03-26 11:40:49 -0400

    implementation of factory pattern
    I have implemented factory pattern.

---
+++ src/layers/presentation/ui/roleDashboardFactory.js
@@ -0,0 +1,52 @@
+const ROLE_TABS = {
+  citizen: ['home', 'recommendations', 'search', 'transit', 'parking', 'activeRental', 'profile'],
+  provider: ['home', 'vehicles', 'rentalData', 'profile'],
+  admin: ['home', 'rentalAnalytics', 'gatewayAnalytics', 'adminDashboard', 'profile'],
+};
+
+class RoleDashboardCreator {
+  createTabs() {
+    throw new Error('createTabs() must be implemented by subclasses.');
+  }
+
+  createMainContent(_renderers) {
+    throw new Error('createMainContent() must be implemented by subclasses.');
+  }
+}
+
+class CitizenDashboardCreator extends RoleDashboardCreator {
+  createTabs() {
+    return ROLE_TABS.citizen;
+  }
+
+  createMainContent(renderers) {
+    return renderers.renderCitizen();
+  }
+}
+
+class ProviderDashboardCreator extends RoleDashboardCreator {
+  createTabs() {
+    return ROLE_TABS.provider;
+  }
+
+  createMainContent(renderers) {
+    return renderers.renderProvider();
+  }
+}
+
+class AdminDashboardCreator extends RoleDashboardCreator {
+  createTabs() {
+    return ROLE_TABS.admin;
+  }
+
+  createMainContent(renderers) {
+    return renderers.renderAdmin();
+  }
+}
+
+export function createRoleDashboardCreator(role) {
+  if (role === 'admin') return new AdminDashboardCreator();
+  if (role === 'provider') return new ProviderDashboardCreator();
+  return new CitizenDashboardCreator();
+}
+
